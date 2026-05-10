a7b3d8341d7cc777e1367cb09d050e1ad0d8dd7d
DarkPanik <misosimple0913@gmail.com>
2026-05-09 20:32:52 -0700

    Did a hello world code. Compiled with GCC and tested that it worked.

---
+++ C_Basics/HelloWorld.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+	/*Just the basic hello world! I saw on the book, effective C, that they use the stslib for the EXIT_SUCCESS.*/
+	printf("Hello, world!");
+	return EXIT_SUCCESS;
+}
