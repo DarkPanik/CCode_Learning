47d6919e707724d645797f7c6aa59debb38ba27f
DarkPanik <misosimple0913@gmail.com>
2026-05-10 00:27:09 -0700

    added a variable part of the code, then decided to follow a book. Hoping this makes me better at C.

--- C_Basics/C_variables.c
+++ C_Basics/C_variables.c
@@ -1,3 +1,19 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-//This is Variables and such
+//Compiled this code and after a few tries, before i decided to just copy it
+//And now it works! credit: Effective C: An Introduction to Professional C Programming by Robert C. Seacord
+
+
+void swap(int *pa, int *pb) { 
+int t = *pa;
+*pa = *pb; 
+*pb = t; 
+}
+int main() {
+int a = 21; 
+int b = 17;
+swap(&a, &b);
+printf("a = %d, b = %d\n", a, b);
+return EXIT_SUCCESS;
+}
